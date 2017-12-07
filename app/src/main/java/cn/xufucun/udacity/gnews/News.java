86b1b17559be5de3e70546be223119990c38c7e9
xufucun <xufucun@gmail.com>
2017-12-07 21:15:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/xufucun/udacity/gnews/News.java
@@ -0,0 +1,8 @@
+package cn.xufucun.udacity.gnews;
+
+/**
+ * Created by xufuc on 2017/12/7.
+ */
+
+public class News {
+}
---
+++ app/src/main/java/cn/xufucun/udacity/gnews/fragment/ThemeFragment.java
@@ -0,0 +1,8 @@
+package cn.xufucun.udacity.gnews.fragment;
+
+/**
+ * Created by xufuc on 2017/12/7.
+ */
+
+public class ThemeFragment {
+}
